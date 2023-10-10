6ad3cca339f90e7ea75405cda4e3ae11eabac457
Vegard <vegard.nyeng@obos.no>
2023-10-10 15:29:35 +0200

    debugging vercel environment

--- app/routes/strava.tsx
+++ app/routes/strava.tsx
@@ -8,23 +8,19 @@ export const loader: LoaderFunction = async () => {
         throw new Error("Missing CLIENT_ID environment variable");
     }
 
-    return { environent: process.env.ENVIRONMENT, client_id: process.env.CLIENT_ID };
+    return { client_id: process.env.CLIENT_ID, vercel_environment: process.env.VERCEL_ENV };
 }
 
 export default function Login() {
-    const { client_id } = useLoaderData<{ environment: string, client_id: string }>();
+    const { client_id, vercel_environment } = useLoaderData<{ client_id: string, vercel_environment: string }>();
 
-    let environment = process.env.ENVIRONMENT;
-    if (process.env.VERCEL_ENV) {
-        environment = process.env.VERCEL_ENV;
+    let base_url = "http://localhost:3000";
+
+    if (vercel_environment) {
+        base_url = vercel_environment;
     }
 
-    if (environment === '' || client_id === '') {
-        throw new Error("Missing environment parameters for either ENVIRONMENT or CLIENT_ID");
-    }
-
-    const base_url = environment === "Production" ? "https://www.fitnessprogress.run/" : "http://localhost:3000";
-    console.log(base_url)
+    console.log("Baseurl", base_url)
 
     const authorizationUrl = `https://www.strava.com/oauth/authorize?client_id=${client_id}&response_type=code&redirect_uri=${base_url}/callback&approval_prompt=auto&scope=read_all,activity:read_all`;
 
