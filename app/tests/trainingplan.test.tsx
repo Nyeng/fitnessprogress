fe6450eac189bf0cdd1af48e9d5fc92feda3e14c
Vegard <vegard.nyeng@obos.no>
2023-11-10 21:15:20 +0100

    removed dummy test to pass checks

--- app/tests/trainingplan.test.tsx
+++ app/tests/trainingplan.test.tsx
@@ -6,8 +6,3 @@ test('Create training plan', () => {
     expect(workout.name).toBe("Evening Walk")
     expect(workout.cooldownKm).toBe(0.5)
 });
-
-test('Failing test just to see if Github action fails and disallows me from merging', () => {
-    expect(1).toBe(2)
-});
-
