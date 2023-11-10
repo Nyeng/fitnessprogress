8c7c22fdeccdb8e0e1fcda911f69054ede53a6fd
Vegard <vegard.nyeng@obos.no>
2023-11-10 21:03:44 +0100

    checking in failing test to see if i can still merge PR

--- app/tests/trainingplan.test.tsx
+++ app/tests/trainingplan.test.tsx
@@ -1,11 +1,13 @@
 import { Workout } from "../calculations/plan";
 import { expect, test } from 'vitest'
 
-
-
 test('Create training plan', () => {
     let workout = new Workout('Evening Walk', {}, 1, 1, 0.5);
     expect(workout.name).toBe("Evening Walk")
     expect(workout.cooldownKm).toBe(0.5)
+});
+
+test('Failing test just to see if Github action fails and disallows me from merging', () => {
+    expect(1).toBe(2)
 });
 
