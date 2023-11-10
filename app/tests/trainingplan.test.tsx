b39637efa4a7d3961a3c30ee978788bab3be74a3
Vegard <vegard.nyeng@obos.no>
2023-11-10 20:33:29 +0100

    created shell for testing using Vite

--- app/tests/trainingplan.test.tsx
+++ app/tests/trainingplan.test.tsx
@@ -1,16 +1,11 @@
 import { Workout } from "../calculations/plan";
+import { expect, test } from 'vitest'
+
+
 
 test('Create training plan', () => {
-
-    
-    
-    let workout3 = new Workout('Evening Walk', {}, 1, 1, 0.5);
-
-    console.log("name", workout3.name)
-    console.log("name", workout3.cooldownKm)
-    console.log("name", workout3.warmupMiles)
-
-
-
-
+    let workout = new Workout('Evening Walk', {}, 1, 1, 0.5);
+    expect(workout.name).toBe("Evening Walk")
+    expect(workout.cooldownKm).toBe(0.5)
 });
+
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-        '^~/(.*)$': '<rootDir>/$1',
-      },
-  };
